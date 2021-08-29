ffd278318746d25b52fe94364fcf7aeb334847af
Denis <Sitronik92@mail.ru>
2021-08-29 17:25:19 +0300

    fix No visible @interface

--- src/ios/PowerManagement.m
+++ src/ios/PowerManagement.m
@@ -1,12 +1,12 @@
 /*
  * Copyright 2013 Wolfgang Koller
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,48 +25,29 @@
 @implementation PowerManagement
 - (void) acquire:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* result = nil;
-    NSString* jsString = nil;
-    NSString* callbackId = command.callbackId;
-    
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
-    
+
+    CDVPluginResult *pluginResult = nil;
     if( ![app isIdleTimerDisabled] ) {
         [app setIdleTimerDisabled:true];
-        
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        jsString = [result toSuccessCallbackString:callbackId];
     }
-    else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer already disabled"];
-        jsString = [result toErrorCallbackString:callbackId];
-    }
-    
-    [self writeJavascript:jsString];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 
 - (void) release:(CDVInvokedUrlCommand*)command
-{    
-    CDVPluginResult* result = nil;
-    NSString* jsString = nil;
-    NSString* callbackId = command.callbackId;
-    
+{
     // Acquire a reference to the local UIApplication singleton
     UIApplication* app = [UIApplication sharedApplication];
-    
+
+
+    CDVPluginResult *pluginResult = nil;
     if( [app isIdleTimerDisabled] ) {
         [app setIdleTimerDisabled:false];
-        
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        jsString = [result toSuccessCallbackString:callbackId];
     }
-    else {
-        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ILLEGAL_ACCESS_EXCEPTION messageAsString:@"IdleTimer not disabled"];
-        jsString = [result toErrorCallbackString:callbackId];
-    }
-    
-    [self writeJavascript:jsString];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 @end
